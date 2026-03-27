fabfe6bd14e21d39c3866a38071b6af0f128b05f
Mukesh Reddy <2483369@cognizant.com>
2026-03-27 15:01:28 +0530

    mukesh bhai pratice

--- backend/src/main/java/com/medi360/BackendApplication.java
+++ backend/src/main/java/com/medi360/BackendApplication.java
@@ -8,7 +8,6 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-		System.out.println("Mukesh");
-		System.out.println("Reddy");
+		System.out.print("mukesh commited");
 	}
 }
