9e0b505dce6b080d748493961a2479ae85876416
nikhil7196 <2483475@cognizant.com>
2026-04-15 17:24:52 +0530

    DTO Request and Response Created

---
+++ backend/src/main/java/com/medi360/DTO/InvoiceDTO.java
@@ -0,0 +1,17 @@
+package com.medi360.DTO;
+
+import com.medi360.entities.Invoice;
+
+public class InvoiceDTO {
+	private Invoice invoice;
+
+	public Invoice getInvoice() {
+		return invoice;
+	}
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
+	
+	
+}
---
+++ backend/src/main/java/com/medi360/DTO/InvoiceResponseDTO.java
@@ -0,0 +1,28 @@
+package com.medi360.DTO;
+
+import com.medi360.entities.Invoice;
+
+public class InvoiceResponseDTO {
+	private Invoice invoice;
+	private int statusCode;
+	private String message;
+	public Invoice getInvoice() {
+		return invoice;
+	}
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
+	public int getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
