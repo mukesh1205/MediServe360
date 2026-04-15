d3e9723925c66ec497290cc970ea8337166225b8
Raghu24092004 <Peddireddy.RaghuVardhanReddy@cognizant.com>
2026-04-15 16:32:06 +0530

    user DTo added

---
+++ backend/src/main/java/com/medi360/DTO/UserDTO.java
@@ -0,0 +1,16 @@
+package com.medi360.DTO;
+
+import com.medi360.entities.User;
+
+public class UserDTO {
+	private User user;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+}
