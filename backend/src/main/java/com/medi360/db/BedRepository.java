cd5c0a0b6bfee99c5fab2988d8802db7d9b6ad1e
mukesh1205 <Madadi.MukeshReddy@cognizant.com>
2026-04-17 18:14:50 +0530

    changes in service and repo by mukesh

--- backend/src/main/java/com/medi360/db/BedRepository.java
+++ backend/src/main/java/com/medi360/db/BedRepository.java
@@ -1,5 +1,7 @@
 package com.medi360.db;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.medi360.entities.Bed;
 
 @Repository
 public interface BedRepository extends JpaRepository<Bed, Integer> {
+
+	List<Bed> findByWardId(int wardId);
 
 }
--- backend/src/main/java/com/medi360/service/WardService.java
+++ backend/src/main/java/com/medi360/service/WardService.java
@@ -1,8 +1,44 @@
 package com.medi360.service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.medi360.db.WardRepository;
+import com.medi360.entities.Ward;
 
 @Service
 public class WardService {
+	
+	private final WardRepository wardRepository;
+	
+	public WardService(WardRepository wardRepository) {
+		this.wardRepository=wardRepository;
+	}
+	
+	public Ward createWard(Ward ward) {
+		return wardRepository.save(ward);
+	}
+	public List<Ward> getAllWard(){
+		return wardRepository.findAll();
+	}
+	public Ward getWardById(int wardId) {
+		return wardRepository.findById(wardId).orElse(null);
+	}
+	public Ward updateWard(int wardId,Ward updatedWard) {
+		Ward existingWard=wardRepository.findById(wardId).orElse(null);
+		
+		if(existingWard!=null) {
+			existingWard.setWardname(updatedWard.getWardname());
+			existingWard.setWardstatus(updatedWard.getWardstatus());
+			existingWard.setWardcapacity(updatedWard.getWardcapacity());
+			return wardRepository.save(existingWard);
+		}
+		return null;
+	}
+	public void deleteWard(int wardId) {
+		wardRepository.deleteById(wardId);
+	}
+	
+	
 
 }
