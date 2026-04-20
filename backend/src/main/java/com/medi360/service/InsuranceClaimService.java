82c1dc9fc6fbdbc61ff42c8c6eb40213a442efa2
nikhil7196 <2483475@cognizant.com>
2026-04-20 18:31:45 +0530

    All 4 entity api's successfully ran by nikhil

--- backend/src/main/java/com/medi360/service/InsuranceClaimService.java
+++ backend/src/main/java/com/medi360/service/InsuranceClaimService.java
@@ -6,15 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.medi360.db.InsuranceClaimRepository;
+import com.medi360.db.PatientRepository;
 import com.medi360.entities.InsuranceClaim;
-import com.medi360.entities.Invoice;
+import com.medi360.entities.Patient;
 
 @Service
 public class InsuranceClaimService {
 	@Autowired
 	InsuranceClaimRepository insuranceClaimRepository;
-	
+	@Autowired
+	PatientRepository patientRepository;
 	public InsuranceClaim addInsuranceClaim(InsuranceClaim insuranceClaim) {
+		int patientId=insuranceClaim.getPatient().getPatientId();
+		Patient patient=patientRepository.findById(patientId).orElseThrow(()->new RuntimeException("Patient not found"));
+		insuranceClaim.setPatient(patient);
 		return this.insuranceClaimRepository.save(insuranceClaim);
 	}
 	
@@ -22,8 +27,8 @@ public class InsuranceClaimService {
 		return this.insuranceClaimRepository.save(insuranceClaim);
 	}
 	
-	public String deleteInsuranceClaim(InsuranceClaim insuranceClaim) {
-		this.insuranceClaimRepository.delete(insuranceClaim);
+	public String deleteInsuranceClaim(int id) {
+		this.insuranceClaimRepository.deleteById(id);
 		return "successfully deleted insuranceClaim";
 	}
 	public List<InsuranceClaim> getAllInsuranceClaims() {
--- backend/src/main/java/com/medi360/service/InvoiceService.java
+++ backend/src/main/java/com/medi360/service/InvoiceService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.medi360.db.InvoiceRepository;
+import com.medi360.db.PatientRepository;
 import com.medi360.entities.Invoice;
 import com.medi360.entities.Patient;
 
@@ -14,8 +15,12 @@ public class InvoiceService {
 	
 	@Autowired
 	InvoiceRepository invoiceRepository;
-	
+	@Autowired
+	PatientRepository patientRepository;
 	public Invoice addInvoice(Invoice invoice) {
+		int patientId=invoice.getPatient().getPatientId();
+		Patient patient=patientRepository.findById(patientId).orElseThrow(()->new RuntimeException("Patient not found"));
+		invoice.setPatient(patient);
 		return this.invoiceRepository.save(invoice);
 	}
 	
@@ -23,8 +28,8 @@ public class InvoiceService {
 		return this.invoiceRepository.save(invoice);
 	}
 	
-	public String deleteInvoice(Invoice invoice) {
-		this.invoiceRepository.delete(invoice);
+	public String deleteInvoice(int id) {
+		this.invoiceRepository.deleteById(id);
 		return "successfully deleted invoice";
 	}
 	public List<Invoice> getAllInvoices() {
