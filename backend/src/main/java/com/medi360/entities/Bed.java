cbf9da5ab53831dcb3f1e24fc11ff07e9492278b
mukesh1205 <Madadi.MukeshReddy@cognizant.com>
2026-04-21 17:51:40 +0530

    mukesh ran successfully

---
+++ backend/src/main/java/com/medi360/entities/Bed.java
@@ -0,0 +1,60 @@
+
+package com.medi360.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Bed {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int bedId;
+	@ManyToOne
+	@JoinColumn(name="ward_id")
+	private Ward ward;
+	
+	@OneToOne
+	@JoinColumn(name="patient_id")
+	private Patient patient;
+	private String bedStatus;
+	public int getBedId() {
+		return bedId;
+	}
+	public void setBedId(int bedId) {
+		this.bedId = bedId;
+	}
+	public Ward getWard() {
+		return ward;
+	}
+	public void setWard(Ward ward) {
+		this.ward = ward;
+	}
+	public Patient getPatient() {
+		return patient;
+	}
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+	public String getBedStatus() {
+		return bedStatus;
+	}
+	public void setBedStatus(String bedStatus) {
+		this.bedStatus = bedStatus;
+	}
+	public Bed(Ward ward, Patient patient, String bedStatus) {
+		super();
+		this.ward = ward;
+		this.patient = patient;
+		this.bedStatus = bedStatus;
+	}
+	public Bed() {
+		super();
+	}
+	
+}
