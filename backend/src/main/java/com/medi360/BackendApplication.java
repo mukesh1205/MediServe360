88717067397ddf137bc2fef0100397798393ce4c
nikhil7196 <2483475@cognizant.com>
2026-04-27 15:34:22 +0530

    Backend Application modified by nikhil

--- backend/src/main/java/com/medi360/BackendApplication.java
+++ backend/src/main/java/com/medi360/BackendApplication.java
@@ -9,9 +9,9 @@ import io.swagger.v3.oas.annotations.info.Info;
 @SpringBootApplication
 @OpenAPIDefinition(
 		info = @Info(
-				title = "Employee API",
+				title = "MediServe360",
 				version = "1.0",
-				description = "API documentation for managing empoloyees"
+				description = "API documentation for managing MediServe360"
 		)
 )
 public class BackendApplication {
