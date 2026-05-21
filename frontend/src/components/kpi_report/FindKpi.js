e5959a63f6d7fc7f927ea4a741fb8df2c2896ce3
Karthikeya Thota <2483504@cognizant.com>
2026-05-21 15:29:48 +0530

    created function components

---
+++ frontend/src/components/kpi_report/FindKpi.js
@@ -0,0 +1,7 @@
+export default function FindKpi(){
+    return(
+        <div>
+            <h1>This is Find Kpi component</h1>
+        </div>
+    )
+}
---
+++ frontend/src/components/kpi_report/UpdateKpi.js
@@ -0,0 +1,7 @@
+export default function UpdateKpi(){
+    return(
+        <div>
+            <h1>This is Update Kpi component</h1>
+        </div>
+    )
+}
